abd904d34a3851e39094bafcea6a4d0285e6660d
kazumatu981 <kazumatu981@gmail.com>
2024-10-19 07:11:18 +0900

    スケルトンを作成

--- src/fraction.ts
+++ src/fraction.ts
@@ -16,5 +16,71 @@ export class Fraction {
     constructor(numerator: number, denominator: number) {
         this._numerator = numerator;
         this._denominator = denominator;
+
+        if (denominator === 0) {
+            throw new Error('分母は0にはなりません。');
+        }
+
+        // 約分します
+        this.normnalize();
+    }
+
+    /**
+     * この分数とotherを足し合わせた結果を返す
+     * @param other 足し合わせる分数
+     * @returns 足し合わせた結果
+     */
+    add(other: Fraction): Fraction {
+        throw new Error('Method not implemented.');
+    }
+
+    /**
+     * この分数からotherを引いた結果を返す
+     * @param other 引く分数
+     * @returns 引いた結果
+     */
+    subtract(other: Fraction): Fraction {
+        throw new Error('Method not implemented.');
+    }
+
+    /**
+     * この分数とotherを掛けた結果を返す
+     * @param other 掛ける分数
+     * @returns 掛けた結果
+     */
+    multiply(other: Fraction): Fraction {
+        throw new Error('Method not implemented.');
+    }
+
+    /**
+     * 他の分数でこの分数を割った結果を返す
+     * @param other 割る分数
+     * @returns 割った結果
+     */
+    divide(other: Fraction): Fraction {
+        throw new Error('Method not implemented.');
+    }
+
+    /**
+     * この分数を約分します。
+     */
+    normnalize(): void {
+        throw new Error('Method not implemented.');
+    }
+
+    /**
+     * 分子を取得
+     * @returns 分子
+     */
+    get numerator(): number {
+        return this._numerator;
+    }
+
+    /**
+     * 分母を取得
+     * @returns 分母
+     */
+    get denominator(): number {
+        return this._denominator;
     }
 }
